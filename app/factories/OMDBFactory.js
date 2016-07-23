ad967538157ebc7c213a45d7aad3fdc07d1cff29
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-07-23 13:14:08 -0500

    added data GET and POST funcitons

--- app/factories/OMDBFactory.js
+++ app/factories/OMDBFactory.js
@@ -1,7 +1,30 @@
 "use strict";
 
-app.factory("OMDBFactory", function() {
+app.factory("OMDBFactory", function($q, $http, MoviesURL) {
+	var newMovie = {
+		title: "Persona",
+		poster: "http://ia.media-imdb.com/images/M/MV5BMTc1OTgxNjYyNF5BMl5BanBnXkFtZTcwNjM2MjM2NQ@@._V1_SX300.jpg",
+		year: "1966",
+		actors: ["Bibi Anderson", "Liv Ullmann"],
+		description: "A nurse is put in charge of an actress who can't talk and finds that the actress's persona is melding with hers.",
+		stars: null,
+		seen: false,
+		uid: null
+	};
+	//get the movie data based on input in the search field.
+	var getMovieByTitle = function(title){
+		console.log("title", title);
+		return new $q((resolve, reject) => {
+			$http.get(`${MoviesURL}/?t=${title}&y=&plot=full&r=json`)
+			.success((movieData) => {
+				resolve(movieData);
+			})
+			.error((error) => {
+				console.log("error getting by title", error);
+				reject(error);
+			});
+		});
+	};
 
-
-	return {}
+	return {newMovie, getMovieByTitle}
 })
--- app/factories/firebaseDataFactory.js
+++ app/factories/firebaseDataFactory.js
@@ -1,1 +1,44 @@
 "use strict";
+
+app.factory("dataFactory", function(FirebaseURL,$q, $http, firebaseAuthFactory) {
+
+	//get movies from firebase
+	var getMovieList = function() {
+		let movies = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FirebaseURL}/movies.json?orderBy="uid"&equalTo"${firebaseAuthFactory.currentUserID}"`)
+			.success((movieObject) => {
+				let movieCollection = movieObject;
+				Object.keys(movieCollection).forEach((key) => {
+					movieCollection[key].id = key;
+					movies.push(movieCollection[key]);
+				});
+				resolve(movieObject);
+			})
+			.error((error) => {
+				reject(error);
+			});
+		});
+	};
+	//add a totally new movie to firebase
+	var postNewMovie = function(newMovie) {
+		return $q((resolve, reject) => {
+			$http.post(
+				`${FirebaseURL}/movies.json`,
+				JSON.stringify(newMovie)
+				)
+			.success((objFromFirebase) => {
+				resolve(objFromFirebase);
+			})
+			.error((error) => {
+				reject(error);
+			});
+		});
+	};
+
+	//PUT function to edit rating
+
+	//DELETE function to remove from DB
+
+	return {getMovieList, postNewMovie}
+})
